d38d724d6f262e52d4e67e3c19a34ab57299bd49
kafatim <kafatimraz2@gmail.com>
2023-04-28 18:37:01 +0300

    landing page using html - css - js
     landing page using html - css - js

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container=document.querySelector('.container');
+
+function phones(phone){//to change phone photo
+img.src=(phone);
+}
+
+function colors(color){
+    container.style.background=color;
+}
